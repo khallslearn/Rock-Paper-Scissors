22be9cf20212373fa8db26b76ceb963a60bd964d
khallslearn <75588413+khallslearn@users.noreply.github.com>
2022-03-25 14:08:21 -0400

    added reset

--- index.js
+++ index.js
@@ -2,46 +2,48 @@ var rps = ["ROCK","PAPER","SCISSORS"]
 
 // Change score for me
 const me = document.getElementById('me')
-let meScore = 0
+let meScore = 1
 
 // Change score for you
 const you = document.getElementById('you')
-let youScore = 0
+let youScore = 1
 
 // function that selects a random pick
 function computerPlay(){
     return rps[Math.floor(Math.random() * rps.length)]
 } 
+function userPlay(){
+  return rps[Math.floor(Math.random() * rps.length)]
+} 
 
 
 //function plays round of game
 function round(playerSelection,computerSelection){
-  var playerSelection = computerPlay();
+    var playerSelection = computerPlay();
     var computerSelection = computerPlay();
-    if (playerSelection ==="ROCK" && computerSelection ==="PAPER") {
+    if (playerSelection === rps[0] && computerSelection ===rps[1]) {
         
         you.textContent = "You: " + youScore ++
-      }else if(playerSelection ==="ROCK" && computerSelection === "SCISSORS") {
+      }else if(playerSelection ===rps[0] && computerSelection === rps[2]) {
        
         me.textContent= "Me: " + meScore ++
       } else if (playerSelection === computerSelection) {
         alert("Tie")
-      }  else if(playerSelection ==="PAPER" && computerSelection === "SCISSORS") {
+      }  else if(playerSelection ===rps[1] && computerSelection === rps[2]) {
             
             you.textContent = "You: " + youScore ++
-      } else if(playerSelection ==="PAPER" && computerSelection === "ROCK") {
+      } else if(playerSelection === rps[1] && computerSelection === rps[0]) {
        
         me.textContent= "Me: " + meScore ++
-      }else if(playerSelection ==="SCISSORS" && computerSelection === "PAPER") {
+      }else if(playerSelection === rps[2] && computerSelection === rps[1]) {
            
             me.textContent= "Me: " + meScore ++
-      }else if(playerSelection ==="SCISSORS" && computerSelection === "ROCK") {
+      }else if(playerSelection === rps[2] && computerSelection === rps[0]) {
                 
                 you.textContent = "You: " + youScore ++
        } else{
           alert("Try Again")
       }
-     
     }
    
   //function runs game
@@ -50,46 +52,60 @@ function round(playerSelection,computerSelection){
     var playerSelection = computerPlay();
     var computerSelection = computerPlay();
     
+    
 };
 //Rock Button
 const rock = document.getElementById('rock')
 rock.addEventListener('click', function game(){
   round(playerSelection,computerSelection)
-  var playerSelection = "ROCK";
+  var playerSelection = rps[0];
   var computerSelection = computerPlay();
   if (meScore === 6) {
     alert("you win")
   }else if(youScore === 6){
     alert("computer is winner")
   }
+  console.log(playerSelection,computerSelection)
 });
 
 // Paper Button
 const paper = document.getElementById('paper')
 paper.addEventListener('click', function game(){
   round(playerSelection,computerSelection)
-  var playerSelection = "PAPER";
+  var playerSelection = rps[1];
   var computerSelection = computerPlay();
   if (meScore === 6) {
     alert("you win")
   }else if(youScore === 6){
     alert("computer is winner")
   }
+  console.log(playerSelection,computerSelection);
 });
 
 // Scissors Button
 const scissors = document.getElementById('scissors')
 scissors.addEventListener('click', function game(){
   round(playerSelection,computerSelection)
-  var playerSelection = "SCISSORS";
+  var playerSelection = rps[2];
   var computerSelection = computerPlay();
   if (meScore === 6) {
     alert("you win")
   }else if(youScore === 6){
     alert("computer is winner")
   }
+  console.log(playerSelection,computerSelection)
 });
+
+//Reset Button
+function clean(){
+  meScore = 0
+  me.textContent= "Me: " + meScore
+  youScore = 0
+  you.textContent = "You: " + youScore 
+ 
+}
 
 
 //stop game
+
 
